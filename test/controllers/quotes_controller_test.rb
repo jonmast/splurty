25a3cdb7f54a636fd309bf92fdae966a88426e5d
Jonathan Mast <jon@padorpers.com>
2015-09-01 20:10:33 -0400

    Show random quote on homepage.

--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
-  test 'has welcome message on homepage' do
+  test 'has a quote on homepage' do
+    Quote.destroy_all!
+    quote = Quote.create!(saying: 'Wise saying', author: 'Yours Truly')
     get :index
-    assert_select 'h1', 'Welcome to my Awesome Web App'
+    assert_select 'h1', quote.saying
+    assert_select 'h2', quote.author
   end
 end
