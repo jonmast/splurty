514d8e3784d4d741b3bfc9c7ee62f417b7f0afc0
Jonathan Mast <jon@padorpers.com>
2015-09-16 19:37:42 -0400

    Use factorygirl for create sample data

---
+++ test/factories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :quote do
+    author 'Gary Vaynerchuck'
+    saying 'I love people, and the hustle.'
+  end
+end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
   test 'unique_tag' do
-    quote = Quote.create(author: 'Ken Mazaika', saying: 'You only yolo once')
-    assert_equal 'KM#' + quote.id.to_s, quote.unique_tag
+    quote = FactoryGirl.create(:quote)
+    assert_equal 'GV#' + quote.id.to_s, quote.unique_tag
   end
 end
